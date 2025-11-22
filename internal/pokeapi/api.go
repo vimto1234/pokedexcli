8b702c7c395505cc5673d6e409583efb59aebc18
vimto1234 <dvd.thwaites@gmail.com>
2025-11-22 10:28:40 +0000

    api class

---
+++ internal/pokeapi/api.go
@@ -0,0 +1,47 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type locationResult struct {
+	Count    int        `json:"count"`
+	Next     string     `json:"next"`
+	Previous string     `json:"previous"`
+	Results  []location `json:"results"`
+}
+
+type location struct {
+	Name string `json:"name"`
+	Url  string `json:url`
+}
+
+var mainConfig config = config{
+	next:     "https://pokeapi.co/api/v2/location-area/",
+	previous: "",
+}
+
+func getLocation(url string) (locationResult, error) {
+
+	location := locationResult{}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return location, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return location, err
+	}
+
+	if err := json.Unmarshal(data, &location); err != nil {
+		return location, err
+	}
+
+	return location, nil
+}
